42838bc0349780348f4f3fc0d2001ee14f182a1a
Usagich <liloddy@gmail.com>
2017-12-22 18:37:10 +0300

    Uploading Confs

---
+++ FileCnfCustom.ps1
@@ -0,0 +1,23 @@
+﻿param
+(
+    [string[]]$ComputerName="localhost"
+)
+
+Configuration CustomFile 
+{ 
+
+    Set-ExecutionPolicy Unrestricted
+    Import-DscResource -ModuleName FileCHecker
+
+    Node $ComputerName 
+    {  
+
+        FileChecker MyCheck
+        {
+            path = "C:\test\1.txt"
+        }
+
+    }
+}
+CustomFile
+Start-DscConfiguration CustomFile -Force
