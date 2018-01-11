ab466176a67a9a2651d53cc4b39a0bbc72a882f3
Usagich <liloddy@gmail.com>
2018-01-11 17:48:10 +0300

    Updated FileContentStandart.ps1 in Azure Automation.

--- FileContentStandart.ps1
+++ FileContentStandart.ps1
@@ -1,33 +1,3 @@
-﻿Configuration File_Task
+workflow FileContentStandart
 {
-
-    param(
-        [string[]]$ComputerName="localhost"
-    )
-
-    Import-DscResource -ModuleName PsDesiredStateConfiguration
-
-    Node $ComputerName 
-    {  
-
-        File FileContent 
-        {
-            Ensure = "Present"
-            DestinationPath = "D:\test\1.txt"
-            Contents = "hello"
-        }
-        Log LoggingForCopy
-        {
-            Message = "Trying to put hello"
-            DependsOn = "[File]FileContent"
-        }
-
-    }
 }
-   
-#set-item wsman:localhost\client\trustedhosts -value *   
-#Run the function to compile the configuration
-File_Task
-#Pass the configuration to the nodes we defined and configure them
-Start-DscConfiguration File_Task -Wait -Verbose
-#Start-DscConfiguration File_Task
