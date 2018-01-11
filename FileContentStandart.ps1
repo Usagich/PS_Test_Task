abfcc2f26a007cbcc882686287851c8a32e7c73e
Usagich <liloddy@gmail.com>
2018-01-11 18:01:01 +0300

    Update FileContentStandart.ps1

--- FileContentStandart.ps1
+++ FileContentStandart.ps1
@@ -1,3 +1,33 @@
-workflow FileContentStandart
+Configuration File_Task
 {
+
+    param(
+        [string[]]$ComputerName="localhost"
+    )
+
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+    Node $ComputerName 
+    {  
+
+        File FileContent 
+        {
+            Ensure = "Present"
+            DestinationPath = "D:\test\1.txt"
+            Contents = "hello"
+        }
+        Log LoggingForCopy
+        {
+            Message = "Trying to put hello"
+            DependsOn = "[File]FileContent"
+        }
+
+    }
 }
+   
+#set-item wsman:localhost\client\trustedhosts -value *   
+#Run the function to compile the configuration
+File_Task
+#Pass the configuration to the nodes we defined and configure them
+Start-DscConfiguration File_Task -Wait -Verbose
+#Start-DscConfiguration File_Task 
