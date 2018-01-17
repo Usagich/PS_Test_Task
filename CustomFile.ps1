6975af9b274bd5accdab62ac574d2aad78930e20
Aliaksandra Navitskaya <Aliaksandra_Navitskaya@epam.com>
2018-01-17 16:16:09 +0300

    a day without git is gone. a lot of stuff

---
+++ CustomFile.ps1
@@ -0,0 +1,24 @@
+﻿Configuration CustomFile 
+{ 
+
+    param
+    (
+       [string[]]$ComputerName="localhost" 
+    ) 
+
+
+    #Set-ExecutionPolicy Unrestricted
+    Import-DscResource -ModuleName FileCHecker
+
+    Node $ComputerName 
+    {  
+
+        FileChecker MyCheck
+        {
+            path = "c:\test\1.txt"
+        }
+
+    }
+}
+#CustomFile
+#Start-DscConfiguration CustomFile -Force -Wait -Verbose
