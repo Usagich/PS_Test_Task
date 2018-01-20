ca923c9832958b16cd689994d395c80442b89ae3
Usagich <liloddy@gmail.com>
2018-01-20 17:07:08 +0300

    Add files via upload

---
+++ Composite Configuration Example/CallingxTask.ps1
@@ -0,0 +1,20 @@
+﻿Configuration CallingxTask
+{
+
+    param(
+        [string]$azureVarPath=(Get-AutomationVariable -Name 'Xpath'),
+        [string]$azureVarFileName=(Get-AutomationVariable -Name 'XfileName'),
+        [string]$azureVarFileContent=(Get-AutomationVariable -Name 'XfileContent')
+    )
+    
+    Import-DscResource -ModuleName 'xTask'
+        
+
+    xTaskCompo ImportantTask
+	{        
+	    Path = $azureVarPath
+	    FileName = $azureVarFileName
+	    FileContent = $azureVarFileContent
+	}
+
+}
